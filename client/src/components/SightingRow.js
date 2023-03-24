50214dd08013229236d96419447dbc7a3848d08b
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-24 08:38:25 -0600

    Formatted date with React Moment

--- client/src/components/SightingRow.js
+++ client/src/components/SightingRow.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Table } from "semantic-ui-react";
+import Moment from 'react-moment';
 
 function SightingRow(props) {
   return (
     <Table.Row>
       <Table.Cell>{props.nickname}</Table.Cell>
       <Table.Cell>{props.species}</Table.Cell>
-      <Table.Cell>{props.date}</Table.Cell>
+      <Table.Cell><Moment format={"MM-DD-YYYY"}>{props.date}</Moment></Table.Cell>
       <Table.Cell>{props.time}</Table.Cell>
       <Table.Cell>{props.longitude}</Table.Cell>
       <Table.Cell>{props.latitude}</Table.Cell>
