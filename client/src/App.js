5e1c5e4755c788fa5e1f9583cca31ee3e60a6066
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-24 09:44:06 -0600

    created basic header

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Sightings from "./components/Sightings";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from Techtonica</h1>
+      <Header />
       <Sightings />
     </div>
   );
---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="headerdiv">
+      <div className="headertext">
+        <h1>Endangered Wildlife Spotters</h1>
+        <h3>Hoping to Conserve Species One Animal at a Time.</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
